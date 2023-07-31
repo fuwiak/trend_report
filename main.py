8f2a8fb1bdcf472e9e92518464467e692aa376d4
fuwiak <poczta130@gmail.com>
2023-08-01 00:02:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import solara
+
+
+color = solara.reactive("red")
+
+
+@solara.component
+def Page():
+    solara.Select(label="Color", values=["red", "green", "blue", "orange"], value=color)
+    solara.Markdown("## Hello World", style={"color": color.value})
