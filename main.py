fd2393a288afc384c6e73e48e061d1a11ff24a84
fuwiak <poczta130@gmail.com>
2023-08-01 02:04:07 +0100

    add Procfile

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import solara
+# import solara
+#
+#
+# color = solara.reactive("red")
+#
+#
+# @solara.component
+# def Page():
+#     solara.Select(label="Color", values=["red", "green", "blue", "orange"], value=color)
+#     solara.Markdown("## Hello World", style={"color": color.value})
+#
+#
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-color = solara.reactive("red")
-
-
-@solara.component
-def Page():
-    solara.Select(label="Color", values=["red", "green", "blue", "orange"], value=color)
-    solara.Markdown("## Hello World", style={"color": color.value})
-
-
+@app.route('/')
+def index():
+  return "Hello World!"
